807eb25e4b5647b8ab5919b289bc87efd481a17a
IvanL <antila3567@gmail.com>
2023-12-25 18:07:02 +0200

    added custom toast to the app

---
+++ ExpenseTracker/Helpers/Toast.swift
@@ -0,0 +1,18 @@
+//
+//  Toast.swift
+//  ExpenseTracker
+//
+//  Created by Иван Легенький on 25.12.2023.
+//
+
+import SwiftUI
+
+struct Toast: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Toast()
+}
